37dfd5c10c42429f350f367fd883467885770d5e
Gökhan Akyil <akyil88@outlook.de>
2023-07-27 13:21:29 +0200

    New files

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// phew… not a lot going on here. Please add some code!
+const bookmarkbutton = document.querySelector('[data-js="bookmark-button"]');
+
+bookmarkbutton.addEventListener("click", () => {
+  bookmarkbutton.classList.toggle("bookmark--active");
+});
+
+const answerButton = document.querySelector('[data-js="answer-button"]');
+const answerParagraph = document.querySelector('[data-js="answer"]');
+
+answerButton.addEventListener("click", () => {
+  answerParagraph.classList.toggle("card__answer--active");
+  if (answerParagraph.classList.contains("card__answer--active")) {
+    answerButton.textContent = "Hide Answer";
+  } else {
+    answerButton.textContent = "Show Answer";
+  }
+});
